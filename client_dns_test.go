5ab2ffb4f7256ebd9950e64edd105d6fcab91e72
d3mondev <55468528+d3mondev@users.noreply.github.com>
2021-03-11 13:44:25 -0500

    Added a sleep to TestClientResolveLarge to make sure we have some concurrency going on

--- client_dns_test.go
+++ client_dns_test.go
@@ -111,15 +111,17 @@ func TestClientResolve(t *testing.T) {
 }
 
 func TestClientResolveLarge(t *testing.T) {
-	resolver := &stubResolver{sleep: time.Duration(0), records: []Record{{Question: "foo.bar", Type: TypeA, Answer: "127.0.0.1"}}}
-	client := newClientDNS(resolver, 3)
+	const iterations int = 32768
 
-	list := make([]string, 32768)
+	resolver := &stubResolver{sleep: time.Duration(10 * time.Microsecond), records: []Record{{Question: "foo.bar", Type: TypeA, Answer: "127.0.0.1"}}}
+	client := newClientDNS(resolver, 10)
+
+	list := make([]string, iterations)
 	for i := range list {
 		list[i] = fmt.Sprintf("query-%d", i)
 	}
 
 	got := client.Resolve(list, TypeA)
 
-	assert.Equal(t, 32768, len(got))
+	assert.Equal(t, iterations, len(got))
 }
